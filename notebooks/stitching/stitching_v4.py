bf6dca48cfd5eb70c311a87ac957610f7e2856b3
lmeyersmt <lucas.meyer@smt.ufrj.br>
2023-09-25 14:34:52 -0300

    Adding stitching_v4.py(santa-cruz)

--- notebooks/stitching/stitching_v4.py
+++ notebooks/stitching/stitching_v4.py
@@ -1,13 +1,13 @@
-import os
-import sys
+# import os
+# import sys
 import cv2
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from PIL import Image
+# import matplotlib.pyplot as plt
+# from PIL import Image
 
-sys.path.append('../../codes')
-from utils import video_utils
+# sys.path.append('../../codes')
+# from utils import video_utils
 
 TUBIACANGA  = '../../Regioes/tubiacanga/'
 PACIENCIA   = '../../Regioes/paciencia/'
@@ -18,28 +18,30 @@ STRING_BODY = 'frame_{}.jpeg'
 IMAGE_PATH = 'frames/'
 DATA_FRAME_PATH = 'dataFrames/'
 
-def cumulative_sum2 (data_frame_path, image_path, translation = 0.2):
+def cumulative_sum2 (data_frame_path, video_path, translation = 0.2):
   df = pd.read_csv(data_frame_path)
   x = df['x']
   y = df['y']
 
-  frame = np.array(Image.open(image_path + '/frame_0001.jpeg'))
+  video = cv2.VideoCapture(video_path)
+  frame = video.read()[1]
   
   y_diff = int(frame.shape[0] - frame.shape[0]*translation)
   x_diff = int(frame.shape[1] - frame.shape[1]*translation)
 
   frames_desloc = []
   counter = 0
-  aux = [0,0]
-  for i in range(x.shape[0]):
+  while counter < df.shape[0]:
+    aux = [0,0]
     while (aux[0] < x_diff) or (aux[1] < y_diff):
-      counter += 1
-      aux[0] += x[counter]
-      aux[1] += y[counter]
-      if (abs(df['y'][counter]) > abs(df['x'][counter])):
-        aux += abs(df['y'][counter])
-    frames_desloc.append([(x,y),counter])
+      if counter < df.shape[0]:
+        aux[0] += x[counter]
+        aux[1] += y[counter]
+        counter+=1
+      else:
+        exit
+    frames_desloc.append([(aux[0],aux[1]),counter])
   return frames_desloc
 
-results = cumulative_sum2(SANTA_CRUZ+'dataFrames/all/phase_correlate.csv', SANTA_CRUZ+)
-print (results)
+df_overlaped = cumulative_sum2('../../Regioes/santa-cruz/dataFrames/all/phase_correlate.csv', '../../Dataset/videos/santa-cruz/20210225_rectified_DJI_0067.avi')
+df_overlaped.to_csv('../../Regioes/santa-cruz/dataFrames/overlaped/phase_correlate02.csv')
