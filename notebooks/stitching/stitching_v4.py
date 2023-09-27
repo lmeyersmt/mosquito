2e972b753deabfc59d1588515a658e3d658ecad4
lmeyersmt <lucas.meyer@smt.ufrj.br>
2023-09-26 21:54:51 -0300

    To compare in my machine

--- notebooks/stitching/stitching_v4.py
+++ notebooks/stitching/stitching_v4.py
@@ -1,5 +1,6 @@
 # import os
 # import sys
+import time
 import cv2
 import numpy as np
 import pandas as pd
@@ -8,7 +9,8 @@ import pandas as pd
 
 # sys.path.append('../../codes')
 # from utils import video_utils
-
+inicio = time.time()
+print(inicio)
 TUBIACANGA  = '../../Regioes/tubiacanga/'
 PACIENCIA   = '../../Regioes/paciencia/'
 SEPETIBA    = '../../Regioes/sepetiba/'
@@ -24,7 +26,9 @@ def cumulative_sum2 (data_frame_path, video_path, translation = 0.2):
   y = df['y']
 
   video = cv2.VideoCapture(video_path)
-  frame = video.read()[1]
+  print(video)
+  frame = video.read()
+  frame = frame[1]
   
   y_diff = int(frame.shape[0] - frame.shape[0]*translation)
   x_diff = int(frame.shape[1] - frame.shape[1]*translation)
@@ -33,7 +37,7 @@ def cumulative_sum2 (data_frame_path, video_path, translation = 0.2):
   counter = 0
   while counter < df.shape[0]:
     aux = [0,0]
-    while (aux[0] < x_diff) or (aux[1] < y_diff):
+    while (abs(aux[0]) < x_diff) or (abs(aux[1]) < y_diff):
       if counter < df.shape[0]:
         aux[0] += x[counter]
         aux[1] += y[counter]
@@ -43,5 +47,7 @@ def cumulative_sum2 (data_frame_path, video_path, translation = 0.2):
     frames_desloc.append([(aux[0],aux[1]),counter])
   return frames_desloc
 
-df_overlaped = cumulative_sum2('../../Regioes/santa-cruz/dataFrames/all/phase_correlate.csv', '../../Dataset/videos/santa-cruz/20210225_rectified_DJI_0067.avi')
-df_overlaped.to_csv('../../Regioes/santa-cruz/dataFrames/overlaped/phase_correlate02.csv')
+df_overlaped = cumulative_sum2('../../Regioes/sepetiba/dataFrames/all/phase_correlate.csv', '../../Dataset/videos/sepetiba/20210317_rectified_DJI_0073.avi')
+fim = time.time()
+print (inicio - fim)
+df_overlaped.to_csv('../../Regioes/sepetiba/dataFrames/overlaped/phase_correlate02.csv')
